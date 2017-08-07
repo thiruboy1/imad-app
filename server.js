b19778d11eefcbadbc77e8ded73a72496b419c8a
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-07 16:23:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('articleone', function(req, res){
+app.get('/articleone', function(req, res){
     res.send('This is first article');
 });
 
-app.get('article-two', function(req, res){
+app.get('/article-two', function(req, res){
     res.send('This is second article');
 });
-app.get('article-three', function(req, res){
+app.get('/article-three', function(req, res){
     res.send('This is third article');
 });
 
