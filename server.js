677eb218563789467d8d4b9c7280bb0174232d06
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-07 21:19:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleName', function(req, res){
+    var articleName = req.params.articleName;
     res.send(createtemplete(articles[articleName]));
 });
 
