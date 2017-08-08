00568c02d4be5ec53a0ae7af3f620fa0e1c4365d
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 23:14:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter',function(req, res){
-    counter = counter+1;
+    counter = counter+2;
     res.send(counter.toString());
     
     
