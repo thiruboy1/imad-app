a6bf24cc8d5c8c0a0f6788d3240403ea04f06df8
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 12:40:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'NEW VALUE';
 
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.onclick = function(){
   img.style.marginleft = '100px';
   
