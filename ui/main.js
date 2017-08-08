7983cec89bf94ecce1e9c71938ccd099cbcff5b5
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 16:19:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,33 @@
+console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.onclick = function(){
+element.innerHTML = 'NEW VALUE';
+};
+
+var img = document.getElementById('madi');
+marginLeft = 0;
+marginRight = 0;
+var moveRight = function(){
+        marginLeft = marginLeft + 10;
+        if(marginLeft<500)
+        {img.style.marginLeft = marginLeft+'px';}
+        else if (marginLeft >= 500 )
+        {img.style.marginLeft = 0; }
+};
+
+img.onclick = function(){var interval = setInterval(moveRight, 10); };
+
+
+
+
+
+
+
+
+
+
 var button = document.getElementById('counter');
-counter = 0
+counter = 0;
 
 button.onclick = function(){
 
