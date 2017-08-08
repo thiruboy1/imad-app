dcab0197bb6e890b66a5da418344c082831c372e
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 14:58:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ element.innerHTML = 'NEW VALUE';
 
 var img = document.getElementById('madi');
 marginLeft = 0;
+marginRight = 0;
 var moveRight = function(){
         marginLeft = marginLeft + 10;
         if(marginLeft<500)
@@ -15,9 +16,20 @@ var moveRight = function(){
   
     
 };
+var moveLeft = function(){
+        marginRight = marginRight + 10;
+        if(marginRight<500)
+        {
+    img.style.marginRight = margiRight+'px';
+            
+        }
+  
+    
+};
 img.onclick = function(){
  
     var interval = setInterval(moveRight, 10); 
+    var interval1 = setInterval(moveLeft, 10); 
     
   img.style.marginLeft = '1px';
   
