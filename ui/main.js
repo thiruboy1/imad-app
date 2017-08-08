a347b9abcc6e22de79d329cfba67423234a52d2d
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 11:41:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 y = alert('do you want to change name');
 
-if (y=y)
+if (y==y)
 {
     element.innerHtml = 'Thiru Vasu'
 }
