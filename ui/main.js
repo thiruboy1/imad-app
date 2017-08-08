5ce5243d7136d64f43827e8e1c10abfcf763ea9d
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 17:50:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@
 var button = document.getElementById('counter');
 
 var request = XMLHttpRequest();
-request.httpRequest.onreadystatechange = function()
+request.onreadystatechange = function()
 {
     // Process the server response here.
    if (request.readyState === XMLHttpRequest.DONE) 
    {
-       if (httpRequest.status === 200)
+       if (request.status === 200)
        {
           var counter = request.responseText; 
           var span = document.getElementById('count');
@@ -20,8 +20,8 @@ request.httpRequest.onreadystatechange = function()
 
 button.onclick = function(){
     
- httpRequest.open('GET', 'http://thiruboy1.imad.hasura-app.io/counter',true);
- httpRequest.send();
+ request.open('GET', 'http://thiruboy1.imad.hasura-app.io/counter',true);
+ request.send();
 
 
 
