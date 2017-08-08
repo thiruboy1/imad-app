3a279918942109c8a10523e32c1f060e0c506339
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-08 16:04:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,14 +26,14 @@ img.onclick = function(){
     var interval = setInterval(moveRight, 10); 
 };
 
-counter = 0;
+count = 0;
 var button = document.getElementById('counter');
 
 button.onclick = function(){
 
-counter = counter+1;
+count = count+1;
 var span = document.getElementById('counID');
-span.innerHTML = counter.toString();
+span.innerHTML = count.toString();
 
 };
 
