76d16c3f8df8d5782d2f39f6dc807f56ce435b2f
thiruboy1 <30684534+thiruboy1@users.noreply.github.com>
2017-08-09 11:28:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ request.onreadystatechange = function()
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    var names = [name1,name2,name3,name4];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for(i=0;i<names.length;i++){
         list += '<li>'+ names[i] +'</li>';
